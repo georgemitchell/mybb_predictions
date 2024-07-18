3e4d3b0a4a5d491af303f08f02dd720db28f5ee5
George Mitchell <gmitchell@riotgames.com>
2024-07-18 08:39:48 -0700

    Added 2024 Schedule

--- data/get_games.py
+++ data/get_games.py
@@ -11,7 +11,7 @@ def convert_time(time_str):
     return eastern
 
 if __name__ == "__main__":
-    year = 2023
+    year = 2024
     team = "Stanford"
     season_type = "regular"
     prefix = "cardbd"
